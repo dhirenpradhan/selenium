cff00c12cfac9b71e6198b7367abde7db5eafa14
dhirenpradhan <106027865+dhirenpradhan@users.noreply.github.com>
2022-05-22 22:09:25 +0530

    Add files via upload

---
+++ coditionalStatement.java
@@ -0,0 +1,21 @@
+ class coditionalStatement {
+     public static void main(String[] args) {
+         int a =255;
+         int b=255;
+         if (a>b){
+             System.out.println("greater");}
+                          
+             
+            
+            else if(a==b){
+             System.out.println("both are equal");
+            }
+        }
+    }
+             
+        
+         
+     
+
+    
+
